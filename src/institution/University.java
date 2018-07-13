beded28031532a6bd4a83d138ce04ce85c65b8c1
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-13 20:04:31 +0300

    add printList function for printing list of students in university, add ArrayList container for students

--- src/institution/University.java
+++ src/institution/University.java
@@ -1,17 +1,29 @@
 package institution;
 
+import java.util.ArrayList;
+
 import person.Student;
 
 public class University {
+	private String nameOfUniversity;
+	private ArrayList<Student> lst;		//list of students
+	
     public University(String name) {
-        //TODO: Implementation is needed
+    	nameOfUniversity = name;
+    	lst = new ArrayList<Student>();
     }
 
     public void setStudent(Student student) {
-        //TODO: Implementation is needed
+        
     }
 
     public void addStudent(Student student) {
-        //TODO: Implementation is needed
+        lst.add(student);		//add student to list
+    }
+    
+    public void printList() {
+    	for(Student std : lst) {
+    		System.out.println(std.getName());
+    	}
     }
 }
