afad62612f77ec82fd6f4242b784dd8fe31a3ae3
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-15 19:15:03 +0300

    add some coments

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -18,8 +18,8 @@ public class Internship {
     }
 
     public void setStudent(Student student) {			//set Student to internship
-        if(student.getKnowledgeLevel() > this.averageKnowledge.getLevel()) {
-        	this.listOfInternshipStudents.add(student);
+        if(student.getKnowledgeLevel() > this.averageKnowledge.getLevel()) {	//check if student knowledge bigger than average knowledge of university
+        	this.listOfInternshipStudents.add(student);							//if bigger than average value then add him to list of internship
         }
     }
 
