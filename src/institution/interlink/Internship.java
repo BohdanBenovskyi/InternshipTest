c1a19c9d6d6ea102d6a1f9850dc610b3a1252020
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-15 18:08:29 +0300

    add students to internship by checking university average knowledge level and students knowledge level, implements method getStudents() which returns string of students in intership

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -1,20 +1,33 @@
 package institution.interlink;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import person.Student;
+import person.consciousness.Knowledge;
 
 public class Internship {
-	public Internship(String name) {
-        //TODO: Implementation is needed
+	private String nameOfInternShip;					//variable which contains name of internship
+	private Knowledge averageKnowledge;					//variable which contains average level of knowledge
+	private List<Student> listOfInternshipStudents;		//variable which contains list of internship students
+	
+	public Internship(String name, List<Student> allStudents, Knowledge averageKnowledge) {
+        this.nameOfInternShip = name;
+        this.averageKnowledge = averageKnowledge;
+        this.listOfInternshipStudents = new ArrayList<Student>();
     }
 
-    public void setStudent(Student student) {
-        //TODO: Implementation is needed
+    public void setStudent(Student student) {			//set Student to internship
+        if(student.getKnowledgeLevel() > this.averageKnowledge.getLevel()) {
+        	this.listOfInternshipStudents.add(student);
+        }
     }
 
     public String getStudents() {
-        //TODO: Implementation is needed
-        return "Andrew Maslenko\nJulia Veselkina\n";
+    	String students = "";
+    	for(Student stnd : this.listOfInternshipStudents) {
+    		students += stnd.getStudentName() + "\n";
+    	}
+    	return students;
     }
 }
