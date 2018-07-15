b06f6ec0e39a535ef346a30d6668ec2e1eab72ca
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-15 19:16:47 +0300

    add more students and test it

--- src/main/Application.java
+++ src/main/Application.java
@@ -10,8 +10,16 @@ public class Application {
         university.addStudent(new Student("Andrew Kostenko", 5));
         university.addStudent(new Student("Julia Veselkina", 4));
         university.addStudent(new Student("Maria Perechrest", 3));
+        university.addStudent(new Student("Petro Nicaragua", 2));
+        university.addStudent(new Student("Vasuluna Zimbabve", 1));
+        university.addStudent(new Student("Alex Uganda", 5));
+        university.addStudent(new Student("Ivan Malaizia", 4));
+        university.addStudent(new Student("Gregory Canada", 4));
 
-        Internship internship = new Internship("Interlink");
+        Internship internship = new Internship("Interlink", university.getAverageKnowledge());
+        for(Student stnd : university.getListOfStudents()) {
+        	internship.setStudent(stnd);
+        }
         System.out.println("List of internship's students:");
         System.out.println(internship.getStudents());
     }
