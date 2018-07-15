1771f8b0758f99c1cbbb89e1a30e7062e42720ad
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-15 17:47:12 +0300

    add calculating of average university knowledge level with adding new student, add methods getAverageKnowledge() and getListOfStudents()

--- src/institution/University.java
+++ src/institution/University.java
@@ -7,19 +7,43 @@ import person.Student;
 import person.consciousness.Knowledge;
 
 public class University {
-	private String nameOfUniversity;
-	private List<Student> listOfStudents;
+	private String nameOfUniversity;				//variable which contains name of university
+	private List<Student> listOfStudents;			//variable which contains list of all students
+	private Knowledge averageUniversityKnowledge;	//variable which contains average level of knowledge
+	private Student bestStudent;					//the best student in all University
 	
 	public University(String name) {
-		nameOfUniversity = name;
-		listOfStudents = new ArrayList<Student>();
+		this.nameOfUniversity = name;
+		this.listOfStudents = new ArrayList<Student>();
+		this.averageUniversityKnowledge = new Knowledge(0);	//zero for empty university
     }
 
     public void setStudent(Student student) {
-        //TODO: Implementation is needed
+        this.bestStudent = student;
     }
 
     public void addStudent(Student student) {
         listOfStudents.add(student);
+        
+        //calculation of average level
+        if(listOfStudents.isEmpty()) {	//if this is our first student than set average level to level knowledge of student
+        	averageUniversityKnowledge.setLevel(student.getKnowledgeLevel());
+        }
+        else {
+        	int averageLevel = 0;		//variable for sum of all students knowledges
+        	for(Student stnd : listOfStudents) {
+        		averageLevel += stnd.getKnowledgeLevel();
+        	}
+        	averageUniversityKnowledge.setLevel((int)averageLevel/listOfStudents.size());	//calc average
+        	System.out.println("Average: " + averageUniversityKnowledge.getLevel());
+        }
+    }
+    
+    public Knowledge getAverageKnowledge() {
+    	return this.averageUniversityKnowledge;
+    }
+    
+    public List getListOfStudents() {
+    	return this.listOfStudents;
     }
 }
