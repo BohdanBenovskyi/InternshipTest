49121f323b4d7cc1bfd530f4606c6580ff6ce165
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-26 15:21:22 +0300

    format code according to Java Code Conventions

--- src/person/consciousness/Knowledge.java
+++ src/person/consciousness/Knowledge.java
@@ -1,17 +1,17 @@
 package person.consciousness;
 
 public class Knowledge {
-	private int lvl;
-	
-	public Knowledge(int level) {
-        lvl = level;
+    private int lvl;
+
+    public Knowledge(int level) {
+	lvl = level;
     }
-	
-	public int getLevel() {
-		return this.lvl;
-	}
-	
-	public void setLevel(int level) {
-		this.lvl = level;
-	}
+
+    public int getLevel() {
+	return this.lvl;
+    }
+
+    public void setLevel(int level) {
+	this.lvl = level;
+    }
 }
