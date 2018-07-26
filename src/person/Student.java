96ad8833552e95fec62d66952853ba6725b8fdbb
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-26 15:21:06 +0300

    format code according to Java Code Conventions

--- src/person/Student.java
+++ src/person/Student.java
@@ -3,23 +3,23 @@ package person;
 import person.consciousness.Knowledge;
 
 public class Student {
-	private Knowledge knowledgeOfStudent;
-	private String nameOfStudent;
-	
-	public Student(String name, int level) {
-		this.nameOfStudent = name;
-		this.knowledgeOfStudent = new Knowledge(level);
+    private Knowledge knowledgeOfStudent;
+    private String nameOfStudent;
+
+    public Student(String name, int level) {
+	this.nameOfStudent = name;
+	this.knowledgeOfStudent = new Knowledge(level);
     }
 
     public void setKnowledge(Knowledge knowledge) {
-        this.knowledgeOfStudent = knowledge;
+	this.knowledgeOfStudent = knowledge;
     }
-    
+
     public int getKnowledgeLevel() {
-    	return this.knowledgeOfStudent.getLevel();
+	return this.knowledgeOfStudent.getLevel();
     }
-    
+
     public String getStudentName() {
-    	return this.nameOfStudent;
+	return this.nameOfStudent;
     }
 }
