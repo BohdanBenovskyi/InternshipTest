1a522606f5ad5369fc34cb715918b70bd8da625f
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-26 15:19:54 +0300

    format code according to Java Code Conventions

--- src/institution/interlink/Internship.java
+++ src/institution/interlink/Internship.java
@@ -7,27 +7,29 @@ import person.Student;
 import person.consciousness.Knowledge;
 
 public class Internship {
-	private String nameOfInternShip;					//variable which contains name of internship
-	private Knowledge averageKnowledge;					//variable which contains average level of knowledge
-	private List<Student> listOfInternshipStudents;		//variable which contains list of internship students
-	
-	public Internship(String name, Knowledge averageKnowledge) {
-        this.nameOfInternShip = name;
-        this.averageKnowledge = averageKnowledge;
-        this.listOfInternshipStudents = new ArrayList<Student>();
+    private String nameOfInternShip;                // variable which contains name of internship
+    private Knowledge averageKnowledge;             // variable which contains average level of knowledge
+    private List<Student> listOfInternshipStudents; // variable which contains list of internship students
+
+    public Internship(String name, Knowledge averageKnowledge) {
+	this.nameOfInternShip = name;
+	this.averageKnowledge = averageKnowledge;
+	this.listOfInternshipStudents = new ArrayList<Student>();
     }
 
-    public void setStudent(Student student) {			//set Student to internship
-        if(student.getKnowledgeLevel() > this.averageKnowledge.getLevel()) {	//check if student knowledge bigger than average knowledge of university
-        	this.listOfInternshipStudents.add(student);							//if bigger than average value then add him to list of internship
-        }
+    public void setStudent(Student student) { // set Student to internship
+	// check if student knowledge bigger than average knowledge of university
+	if (student.getKnowledgeLevel() > this.averageKnowledge.getLevel()) {
+	    // if bigger than average value then add him to list of internship
+	    this.listOfInternshipStudents.add(student);
+	}
     }
 
     public String getStudents() {
-    	String students = "";
-    	for(Student stnd : this.listOfInternshipStudents) {
-    		students += stnd.getStudentName() + "\n";
-    	}
-    	return students;
+	String students = "";
+	for (Student stnd : this.listOfInternshipStudents) {
+	    students += stnd.getStudentName() + "\n";
+	}
+	return students;
     }
 }
