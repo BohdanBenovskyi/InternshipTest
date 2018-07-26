8ae6413b20d346f7fdbdc5ca513c274e7580e0df
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-26 15:23:28 +0300

    implement adding students from another sources (file, database ...)

--- src/main/Application.java
+++ src/main/Application.java
@@ -5,22 +5,19 @@ import institution.interlink.Internship;
 import person.Student;
 
 public class Application {
-	public static void main(String[] args) {
-        University university = new University("CH.U.I.");
-        university.addStudent(new Student("Andrew Kostenko", 5));
-        university.addStudent(new Student("Julia Veselkina", 4));
-        university.addStudent(new Student("Maria Perechrest", 3));
-        university.addStudent(new Student("Petro Nicaragua", 2));
-        university.addStudent(new Student("Vasuluna Zimbabve", 1));
-        university.addStudent(new Student("Alex Uganda", 5));
-        university.addStudent(new Student("Ivan Malaizia", 4));
-        university.addStudent(new Student("Gregory Canada", 4));
+    public static void main(String[] args) {
+	University university = new University("CH.U.I.");
 
-        Internship internship = new Internship("Interlink", university.getAverageKnowledge());
-        for(Student stnd : university.getListOfStudents()) {
-        	internship.setStudent(stnd);
-        }
-        System.out.println("List of internship's students:");
-        System.out.println(internship.getStudents());
+	university.getStudentsFromSomeSource("DEMONSTRATION"); // or FILE or DATABASE or SERVICE
+	university.setAverageKnowledge(university.getListOfStudents());
+
+	Internship internship = new Internship("Interlink", university.getAverageKnowledge());
+
+	for (Student stnd : university.getListOfStudents()) {
+	    internship.setStudent(stnd);
+	}
+
+	System.out.println("List of internship's students:");
+	System.out.println(internship.getStudents());
     }
 }
