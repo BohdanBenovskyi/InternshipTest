cfba1b770c55c58bd8ef3685b14e4e083d408c85
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-27 16:36:20 +0300

    implements interface Source for files

---
+++ src/source/implementations/FileSource.java
@@ -0,0 +1,16 @@
+package source.implementations;
+
+import java.util.ArrayList;
+
+import person.Student;
+import source.Source;
+
+public class FileSource implements Source {
+
+    @Override
+    public ArrayList<Student> getStudents() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
