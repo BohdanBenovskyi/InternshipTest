33ca8f9b10d8d516e839a5895d95f2f5a62b435c
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-07-27 16:36:54 +0300

    implements interface Source for services

---
+++ src/source/implementations/ServiceSource.java
@@ -0,0 +1,16 @@
+package source.implementations;
+
+import java.util.ArrayList;
+
+import person.Student;
+import source.Source;
+
+public class ServiceSource implements Source {
+
+    @Override
+    public ArrayList<Student> getStudents() {
+	// TODO Auto-generated method stub
+	return null;
+    }
+
+}
